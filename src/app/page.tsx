960d44d9095bb9fa8ca71782fc7a39d075a955d0
Faizan Naseer <f.naseer@mail.utoronto.ca>
2025-03-10 13:53:22 -0400

    revert back to no border

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,19 +8,19 @@ import { motion } from "motion/react";
 
 export default function Home() {
   return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 0.05 }}
-      className="sm:py-12 sm:px-12 sm:border-gray-800 sm:rounded-sm sm:border-[1.5px] sm:shadow-lg sm:bg-[#F4F4F4] border-none p-0"
-    >
-      <div className="flex flex-col gap-4">
-        <Intro />
-        <Projects /> {/* TODO: Add experience section here, that changes back and forth between experience and projects */}
-        <WobblyLine />
-        <Links />
-      </div>
-    </motion.div>
+    // <motion.div
+    //   initial={{ opacity: 0 }}
+    //   animate={{ opacity: 1 }}
+    //   transition={{ duration: 0.05 }}
+    //   className="sm:py-12 sm:px-12 sm:border-gray-800 sm:rounded-sm sm:border-[1.5px] sm:shadow-lg sm:bg-[#F4F4F4] p-0"
+    // >
+    <div className="flex flex-col gap-4">
+      <Intro />
+      <Projects /> {/* TODO: Add experience section here, that changes back and forth between experience and projects */}
+      <WobblyLine />
+      <Links />
+    </div>
+    // </motion.div>
 
   );
 }
