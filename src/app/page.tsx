57b5c6ba62510b60cb4fb2ac42643d38c663caff
Faizan Naseer <f.naseer@mail.utoronto.ca>
2025-03-10 01:44:49 -0400

    added border to webpage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,26 @@
+'use client'
 import Intro from "@/components/Intro";
 import Projects from "@/components/Projects";
 import Links from "@/components/Links";
 import WobblyLine from "@/components/WobblyLine";
+import { motion } from "motion/react";
 
 
 export default function Home() {
   return (
-    <div className="flex flex-col gap-4">
-      <Intro />
-      <Projects /> {/* TODO: Add experience section here, that changes back and forth between experience and projects */}
-      <WobblyLine />
-      <Links />
-    </div>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.05 }}
+      className="py-12 px-12 border-gray-800 rounded-sm border-[1.5px] shadow-lg bg-[#F4F4F4]"
+    >
+      <div className="flex flex-col gap-4">
+        <Intro />
+        <Projects /> {/* TODO: Add experience section here, that changes back and forth between experience and projects */}
+        <WobblyLine />
+        <Links />
+      </div>
+    </motion.div>
+
   );
 }
